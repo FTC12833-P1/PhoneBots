397b5198e66265d4fafe4ae03e6283aedeaef3e2
lincoln12833 <Lincolnbruning@protonmail.com>
2024-06-20 18:43:31 -0400

    finish dump truck

--- teamcodeClassic/opmodes12833/misc/TeleOp_DumpTruck.java
+++ teamcodeClassic/opmodes12833/misc/TeleOp_DumpTruck.java
@@ -31,17 +31,17 @@ public class TeleOp_DumpTruck extends LinearOpMode {
 
 
         while (opModeIsActive()){
-            leftpower = Range.clip(-gamepad1.left_stick_y + gamepad1.right_stick_x, -1, 1);
-            rightPower = Range.clip(-gamepad1.left_stick_y - gamepad1.right_stick_x, -1, 1 );
+            leftpower = Range.clip(-gamepad1.left_stick_y + gamepad1.right_stick_x, -.6, .6);
+            rightPower = Range.clip(-gamepad1.left_stick_y - gamepad1.right_stick_x, -.6, .6 );
 
             leftDrive.setPower(leftpower);
             rightDrive.setPower(rightPower);
 
-            if(gamepad1.x){
-                dumper.setPosition(.56);
+            if(gamepad1.left_bumper){
+                dumper.setPosition(.6);
             }
-            if(gamepad1.y){
-                dumper.setPosition(0);
+            if(gamepad1.right_bumper){
+                dumper.setPosition(1);
             }
         }
     }
@@ -52,6 +52,6 @@ public class TeleOp_DumpTruck extends LinearOpMode {
         leftDrive.setDirection(DcMotorEx.Direction.REVERSE);
 
         dumper = hardwareMap.get(Servo.class, "dumper");
-        dumper.setPosition(0);
+        dumper.setPosition(1);
     }
 }
