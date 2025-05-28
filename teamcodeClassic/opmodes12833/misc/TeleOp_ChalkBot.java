8c86ef239e72fde6270fe13926af9569c40ba2e3
ftc12833-p2 <ftc12833p2@gmail.com>
2025-05-27 20:30:29 -0400

    chalk bot going to git

--- teamcodeClassic/opmodes12833/misc/TeleOp_ChalkBot.java
+++ teamcodeClassic/opmodes12833/misc/TeleOp_ChalkBot.java
@@ -2,20 +2,32 @@ package org.firstinspires.ftc.PhoneBots.teamcodeClassic.opmodes12833.misc;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.exception.RobotCoreException;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="dumpTruck", group="mm")
+@TeleOp(name="chalkBot", group="mm")
 public class TeleOp_ChalkBot extends LinearOpMode {
 
     private DcMotorEx leftDrive;
     private DcMotorEx rightDrive;
+    private Servo tail;
 
     double leftpower;
     double rightPower;
-    private ElapsedTime dumpTime = new ElapsedTime();
+    final double MAX_POWER = 0.4;
+    final double MAX_SLOW_POWER = 0.3;
+    private Gamepad previousGamepad1 = new Gamepad();
+    private Gamepad currentGamepad1 = new Gamepad();
+
+    private ElapsedTime wagTime = new ElapsedTime();
+
+    private boolean needToWagToZero = false;
+
+    private boolean slow = false;
 
     @Override
     public void runOpMode(){
@@ -27,18 +39,53 @@ public class TeleOp_ChalkBot extends LinearOpMode {
 
         waitForStart();
 
+        //wagTime.startTime();
 
         while (opModeIsActive()){
-            leftpower = Range.clip(-gamepad1.left_stick_y + gamepad1.right_stick_x, -.6, .6);
-            rightPower = Range.clip(-gamepad1.left_stick_y - gamepad1.right_stick_x, -.6, .6 );
+            try {
+                previousGamepad1.copy(currentGamepad1);
+            } catch (RobotCoreException e) {
+                e.printStackTrace();
+            }
+            try {
+                currentGamepad1.copy(gamepad1);
+            } catch (RobotCoreException e) {
+                e.printStackTrace();
+            }
+
+            if (currentGamepad1.a && !previousGamepad1.a && !gamepad1.start){
+                slow = !slow;
+            }
+
+            if (slow) {
+                leftpower = Range.clip(gamepad1.left_stick_y - gamepad1.right_stick_x, -MAX_SLOW_POWER, MAX_SLOW_POWER);
+                rightPower = Range.clip(gamepad1.left_stick_y + gamepad1.right_stick_x, -MAX_SLOW_POWER, MAX_SLOW_POWER);
+            } else {
+                leftpower = Range.clip(gamepad1.left_stick_y - gamepad1.right_stick_x, -MAX_POWER, MAX_POWER);
+                rightPower = Range.clip(gamepad1.left_stick_y + gamepad1.right_stick_x, -MAX_POWER, MAX_POWER);
+            }
 
             leftDrive.setPower(leftpower);
             rightDrive.setPower(rightPower);
+
+            if (wagTime.milliseconds() >= 500) {
+                needToWagToZero = !needToWagToZero;
+                wagTime.reset();
+            }
+
+            if (needToWagToZero) {
+                tail.setPosition(0);
+            } else {
+                tail.setPosition(1);
+            }
         }
     }
     public void initHardware(){
         leftDrive = hardwareMap.get(DcMotorEx.class, "left");
         rightDrive = hardwareMap.get(DcMotorEx.class, "right");
         leftDrive.setDirection(DcMotorEx.Direction.REVERSE);
+
+        tail = hardwareMap.get(Servo.class, "tail");
+        tail.setPosition(0);
     }
 }
